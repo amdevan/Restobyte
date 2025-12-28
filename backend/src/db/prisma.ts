68320949f9a94141e3133560384381d8fc2bdfd8
Devan <devanthakur@Devans-MacBook-Air.local>
2025-12-28 21:53:14 +0545

    chore: configure frontend for Coolify and update dependencies

--- backend/src/db/prisma.ts
+++ backend/src/db/prisma.ts
@@ -1,6 +1,6 @@
 import 'dotenv/config';
-import { PrismaClient } from '@prisma/client/index.js';
+import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({});
 
 export default prisma;
